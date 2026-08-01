324ecbc09e7a5dd06225671e0c025fd51a9e9b1b
Venu <venu16046016@gmail.com>
2026-08-02 00:56:31 +0530

    build fix

--- src/shared/components/Footer/footer.jsx
+++ src/shared/components/Footer/footer.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./footer.css";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 // const popularRestaurants = [
 //   "Sharma Snacks",
