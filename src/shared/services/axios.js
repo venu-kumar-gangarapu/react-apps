eb37f2c0697652537d7f2dc030c79d3577d0dc37
Venu <venu16046016@gmail.com>
2026-08-20 09:47:04 +0530

    api url

--- src/shared/services/axios.js
+++ src/shared/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/ordernowApi/v1",
+    baseURL: "https://order-now-app-api.onrender.com/ordernowApi/v1",
 });
 
 export default api;
